e036cf4f3eeee6de33f6fb7e897929061c6360ec
DeviSriSaiCharan <devisrisaicharan@gmail.com>
2024-06-20 20:20:52 +0530

    Fixed the profile image according to his/her name's first letter

--- Frontend/src/mainComponents/Dashboard.jsx
+++ Frontend/src/mainComponents/Dashboard.jsx
@@ -7,37 +7,38 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 import Transactions from "./Transactions";
 import Users from "./Users";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import axios from "axios";
-import SheetBody from './SheetBody'
+import SheetBody from "./SheetBody";
 
 export default function Dashboard() {
-  
-  const [username, setUsername] = useState('')
-  const [balance, setBalance] = useState()
-  
-  async function  getUserData(){
-    try{
-      const jwt = localStorage.getItem("token") || '';
-      const response = await axios.get('http://localhost:3000/account/balance', {
-        headers : {
-          Authorization: `Bearer ${jwt}`
+  const [username, setUsername] = useState("");
+  const [balance, setBalance] = useState();
+
+  async function getUserData() {
+    try {
+      const jwt = localStorage.getItem("token") || "";
+      const response = await axios.get(
+        "http://localhost:3000/account/balance",
+        {
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
         }
-      })
+      );
       // console.log(response.data)
-      setUsername(response.data.username)
-      setBalance(response.data.balance)
-    }
-    catch(err){
-      console.log(err)
+      setUsername(response.data.username);
+      setBalance(response.data.balance);
+    } catch (err) {
+      console.log(err);
     }
   }
-  useEffect(() =>{
-    getUserData()
-  }, [])
+  useEffect(() => {
+    getUserData();
+  }, []);
 
   return (
     <div className="h-full">
@@ -46,14 +47,16 @@ export default function Dashboard() {
         <div className="flex  gap-4 h-12 px-2 justify-between items-center">
           <h3 className="text-lg">{username}</h3>
           <Sheet>
-            <SheetTrigger className="bg-gray-100 border-[1px] border-black rounded-full w-12 h-full flex justify-center items-center text-2xl">
-              U
+            <SheetTrigger className="bg-gray-100 border-[1px] border-black rounded-full w-12 h-full flex justify-center items-center font-semibold text-2xl">
+              {username && username[0].toUpperCase()}
             </SheetTrigger>
             <SheetContent>
               <SheetHeader>
                 <SheetTitle className="text-2xl">Profile</SheetTitle>
                 <SheetDescription>
-                  <div><h1 className="text-xl">{username}</h1></div>
+                  <div>
+                    <h1 className="text-xl">{username}</h1>
+                  </div>
                 </SheetDescription>
               </SheetHeader>
               <SheetBody />
@@ -62,7 +65,7 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="p-4 my-4">
-        <p className="text-lg font-semibold">Your Balance: {balance} ₹</p>
+        <p className="text-lg font-semibold">Your Balance: {balance} ₹</p>   
       </div>
       <div className=" ">
         <Tabs defaultValue="users" className="w-full  p-4">
@@ -71,7 +74,7 @@ export default function Dashboard() {
             <TabsTrigger value="transactions">Transactions</TabsTrigger>
           </TabsList>
           <TabsContent value="users">
-            <Users setBalance={setBalance} username={username}/>
+            <Users setBalance={setBalance} username={username} />
           </TabsContent>
           <TabsContent value="transactions">
             <Transactions />
